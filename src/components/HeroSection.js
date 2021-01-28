9569efaa18087e34041712f89ae0aa38f07c784c
Barcellos-Felipe <barcellossfelipe@gmail.com>
2021-01-27 22:54:51 -0400

    ..

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ function HeroSection () {
   return (
     <div className="hero-container">
       <img src="./pub-media/img/img-home.jpg" alt="" className="home-img"/>
-      <h1 id="hero-section">SELTON BABAQUINHA</h1>
+      <h1 id="hero-section">SOLUÇÕES DIGITAIS PARA O SEU NEGÓCIO</h1>
       <p>Quando o assunto é encontrar maneiras de aumentar sua visibilidade no mercado, nós projetamos as melhores estratégias de uma forma divertida e descontraída.</p>
     </div>
   ); 
